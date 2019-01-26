7fb400de9e46c81a3d021b6e803dc147b991ef7e
Tristan Louet <tristan.louet.dev@gmail.com>
2019-01-26 11:47:07 +0100

    Started world generation code

--- Assets/Scripts/World/World.cs
+++ Assets/Scripts/World/World.cs
@@ -1,7 +1,12 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+using Random = UnityEngine.Random;
 
 public class World : MonoBehaviour
 {
+    // Utils
+
     private static World worldInstance = null;
     static World Get()
     {
@@ -17,11 +22,53 @@ public class World : MonoBehaviour
         return worldInstance;
     }
 
-    void Start()
-    {   
+    // Internal types
+
+    public struct Tile
+    {
+        public enum Type
+        {
+            Grass,
+            Snow,
+            Mountain,
+            Fire
+        }
+
+        public Vector2 coordinates { get; private set; }
+        public Type type { get; set; }
+
+        public Tile(Vector2 coordinates, Type type)
+        {
+            this.coordinates = coordinates;
+            this.type = type;
+        }
     }
 
-    void Update()
+    // World members
+
+    public Dictionary<Vector2, Tile> tiles { get; private set; }
+
+    // Leave the seed to 0 for using the current time. Provide a hardcoded seed otherwise.
+    [SerializeField]
+    private int seed = 0;
+
+    void Start()
     {
+        if (seed == 0)
+        {
+            // TODO TickCount is not the best to use
+            GenerateWorld(Environment.TickCount);
+        }
+        else
+        {
+            GenerateWorld(seed);
+        }
+    }
+
+    void GenerateWorld(int seed)
+    {
+        Random.InitState(seed);
+
+
     }
 }
---
+++ Assets/Scripts/World/WorldGenerationParameters.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "WorldGenerationParameters", menuName = "World/Generation Parameters")]
+public class WorldGenerationParameters : ScriptableObject
+{
+    public int startingWood;
+    public int minWoodDistance;
+    public int maxWoodDistance;
+    public int minWoodRadius;
+    public int maxWoodRadius;
+}
