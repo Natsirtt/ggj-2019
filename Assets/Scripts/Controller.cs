dcee92e5c6924fcae8ea1c9b457642b2c8eac91f
unknown <mattias.lindblad@live.se>
2019-01-26 19:25:07 +0100

    Added rudimentary camera controls

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -4,12 +4,46 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    System.Nullable<Vector3> DragCameraInputLocation;
+    System.Nullable<Vector3> DragCameraStartLocation;
+
     void Update()
     {
-        if(Input.GetButtonDown("PlaceCampFire"))
+        Vector2 mousePosInWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetButtonDown("PlaceCampFire"))
         {
-            Vector2 mousePosInWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             World.Get().SpawnCampFire(mousePosInWorld);
+        }
+
+        if(Input.GetButtonDown("DragCamera"))
+        {
+            DragCameraInputLocation = Input.mousePosition;
+            DragCameraStartLocation = Camera.main.transform.position;
+        }
+
+        if(DragCameraInputLocation != null)
+        {
+            Vector2 dragV = DragCameraInputLocation.Value - Input.mousePosition;
+            dragV *= Camera.main.orthographicSize;
+            float ratio = Camera.main.scaledPixelWidth / Camera.main.scaledPixelHeight;
+            dragV.y *= ratio;
+            dragV /= 200.0f;
+
+            float oldZ = Camera.main.transform.position.z;
+            Camera.main.transform.position = new Vector3(dragV.x, dragV.y, 0) + DragCameraStartLocation.Value;
+        }
+
+        if(Input.GetButtonUp("DragCamera"))
+        {
+            DragCameraInputLocation = null;
+            DragCameraStartLocation = null;
+        }
+
+        float scrollAmount = Input.GetAxis("Mouse ScrollWheel");
+        if (Mathf.Abs(scrollAmount) > 0.0f)
+        {
+            Camera.main.orthographicSize -= scrollAmount;
+            Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, 80, 700);
         }
     }
 }
