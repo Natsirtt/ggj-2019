41ea0861da42e812b87f0c304bcfd4ded8257b91
Tristan Louet <tristan.louet.dev@gmail.com>
2019-01-26 11:09:09 +0100

    Created a World script manager and made it accessible from any other script

---
+++ Assets/Scripts/World/World.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    private static World worldInstance = null;
+    static World Get()
+    {
+        if (worldInstance == null)
+        {
+            worldInstance = FindObjectOfType<World>();
+            if (worldInstance == null)
+            {
+                var newWorld = new GameObject("World");
+                worldInstance = newWorld.AddComponent<World>();
+            }
+        }
+        return worldInstance;
+    }
+
+    void Start()
+    {   
+    }
+
+    void Update()
+    {
+    }
+}
