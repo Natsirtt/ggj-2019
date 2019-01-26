6daf64ce9a253cdb6b00686ed128eec6d57574b2
unknown <mattias.lindblad@live.se>
2019-01-26 10:45:24 +0100

    Added simple agent movement

--- Assets/Scripts/Agent/AgentMovement.cs
+++ Assets/Scripts/Agent/AgentMovement.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class AgentMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 AccumulatedInput;
+    private Vector2 Velocity;
+    public float MaxSpeed = 5.0f;
+
+    public void AddMovementInput(Vector2 inputVector)
     {
-        
+        AccumulatedInput += inputVector;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        HandleInput();
+        HandleTranslation();
+    }
+
+    void HandleInput()
+    {
+        if(AccumulatedInput.magnitude > 0.0f) // accelerate
+        {
+            Vector2 normalizedInput = AccumulatedInput.normalized;
+
+            Velocity += normalizedInput * MaxSpeed;
+            Velocity = Velocity.normalized * Mathf.Min(MaxSpeed, Velocity.magnitude);
+        }
+        else // break
+        {
+            Velocity = Vector2.zero;
+        }
+
+        AccumulatedInput = Vector2.zero;
+    }
+    
+    void HandleTranslation()
+    {
+        transform.position += new Vector3(Velocity.x, Velocity.y, 0) * Time.deltaTime;
     }
 }
