208b41c641fdea8d60f01c1ae4ef365319c82665
Joschka Pottgen <j.lingemann@gmail.com>
2019-01-26 10:39:21 +0100

    Add simple inventory data holder

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField]
+    private int StartingWood;
+
+    private int currentWood;
+    public int CurrentWood
+    {
+        get { return currentWood; }
+        set { currentWood = value;  }
+    }
+
+    public void addWood(int amount)
+    {
+        currentWood += amount;
+    }
+
+    public void removeWood(int amount)
+    {
+        currentWood -= amount;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentWood = StartingWood;   
+    }
+}
