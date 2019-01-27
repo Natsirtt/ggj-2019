69a4e0df8c4ab959a55fb6cc5c6eaf0d3b25215b
unknown <mattias.lindblad@live.se>
2019-01-27 13:01:17 +0100

    Added wind audio

---
+++ Assets/AmbientAudio.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientAudio : MonoBehaviour
+{
+    public List<AudioClip> LoopingClips = new List<AudioClip>();
+    public AudioSource PlayAudioSource;
+    private AudioClip CurrentClip;
+
+    void Start()
+    {
+        ChangeAudioClip();
+    }
+
+    void ChangeAudioClip()
+    {
+        if (LoopingClips.Count > 0)
+        {
+            CurrentClip = LoopingClips[UnityEngine.Random.Range(0, LoopingClips.Count)];
+            if (PlayAudioSource != null)
+            {
+                PlayAudioSource.clip = CurrentClip;
+                PlayAudioSource.Play();
+                PlayAudioSource.loop = false;
+                Invoke("ChangeAudioClip", CurrentClip.length);
+            }
+        }
+    }
+}
